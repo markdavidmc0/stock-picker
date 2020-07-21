5e0bb6683f283985e4686498f2ed2adccacfc440
markdavidmc0 <mark@dotmodus.com>
2020-07-21 21:37:36 +0200

    Added base connector script that downloads stock data from API.

---
+++ connector.py
@@ -0,0 +1,19 @@
+"""Stock picker that conectors to the marketstack API to obtain EOD stock prices."""
+import requests
+
+class Fetcher:
+    """Downloads stock prices from a master index."""
+    
+    def __init__(self):
+        self.data = []
+
+    def download_end_of_day(self):
+        """Download end of day stock data object."""
+        payload = {'access_key': '0ca9e3268525dc676972e5c9b05047cf', 'symbols': 'AAPL'}
+        output = requests.get('http://api.marketstack.com/v1/eod', params=payload)
+        return output
+    
+
+if __name__== '__main__':
+    output = Fetcher().download_end_of_day()
+    print(output)
